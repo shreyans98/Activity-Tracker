ac2a1146b0e929018dd981bc6e14bd0ed3aa6b00
Shreyans Singhi <42538991+shreyans98@users.noreply.github.com>
2021-01-06 14:28:48 +0530

    Index.js file is added

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+//import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <FirebaseContext.Provider value={new Firebase()}>
+     <App />
+
+  </FirebaseContext.Provider>,
+   
+ 
+  document.getElementById('root')
+);
+
