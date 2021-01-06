c226f468cebd3557b99390f348b7abba2e23c256
Shreyans Singhi <42538991+shreyans98@users.noreply.github.com>
2021-01-06 23:33:49 +0530

    Create index.js

---
+++ src/components/Copyright/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Created by '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
+
+export default Copyright;
