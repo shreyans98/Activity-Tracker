4eda45002b30ac8d8bdcb5f420b63fc853f1d1a8
Shreyans Singhi <42538991+shreyans98@users.noreply.github.com>
2021-01-06 14:30:06 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import SignIn from './pages/SignIn'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './config/theme.config';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path="/">
+          <SignIn />
+        </Route>
+        <Route path="/dashboard">
+        <Dashboard />
+        </Route>
+
+        <Route path="/sign-up">
+          <SignUp />
+        </Route>
+      </Switch>
+      </ThemeProvider>
+    </Router>
+    
+  );
+}
+
+export default App;
